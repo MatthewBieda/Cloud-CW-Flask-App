b8d60a9d6f0b5c1c146ef0831dd81e4b7e85db8f
MatthewBieda <63665300+MatthewBieda@users.noreply.github.com>
2021-04-05 14:43:25 +0100

    Reverted Home to Index, changed to AWS RDS database

--- app/queries.py
+++ app/queries.py
@@ -14,7 +14,7 @@ def borough():
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- config.py
+++ config.py
@@ -3,5 +3,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql://root:HelloWorld123@localhost/tflappdatabase'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql://root:HelloWorld123@tflappdatabase.cceacacihiv0.us-east-1.rds.amazonaws.com/tflappdatabaseRDS'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
