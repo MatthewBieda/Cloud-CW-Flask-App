a21619267be3161ffd9f78c4eb77f75b78f6c647
MatthewBieda <63665300+MatthewBieda@users.noreply.github.com>
2021-04-05 17:00:57 +0100

    Added account deletion, changed edit to update and put a back button on accident data page

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+from re import error
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from app import app, db
@@ -83,20 +84,33 @@ def user(username):
     ]
     return render_template('user.html', user=user, posts=posts)
 
-@app.route('/edit_profile', methods=['GET', 'POST'])
+@app.route('/index/user/<username>/delete', methods=['GET','POST', 'DELETE'])
 @login_required
-def edit_profile():
+def delete(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    if request.method == 'POST':
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+            return redirect('/index')
+        return redirect('/404')
+ 
+    return render_template('delete.html', user=user)
+
+@app.route('/update_profile', methods=['GET', 'POST', 'PUT'])
+@login_required
+def update_profile():
     form = EditProfileForm(current_user.username)
     if form.validate_on_submit():
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
         db.session.commit()
         flash('Your changes have been saved.')
-        return redirect(url_for('edit_profile'))
+        return redirect(url_for('update_profile'))
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
-    return render_template('edit_profile.html', title='Edit Profile', form=form)
+    return render_template('update_profile.html', title='Update Profile', form=form)
 
 
 
